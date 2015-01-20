56498f41e989d48eabc864cacc751ac3428838de
Vincent DURMONT <vdurmont@gmail.com>
2015-01-20 11:41:45 -0800

    Initial commit

---
+++ src/main/java/com/vdurmont/elapsedtime/App.java
@@ -0,0 +1,13 @@
+package com.vdurmont.elapsedtime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
